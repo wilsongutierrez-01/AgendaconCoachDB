6cfde82ec0b687b398ab517ba346774d8653b4c9
wilsongutierrez-01 <adolfoguti001@outlook.com>
2022-04-23 11:22:42 -0600

    Terminacion del proyecto

---
+++ app/src/main/java/com/ugb/miprimerproyecto/utilidades.java
@@ -0,0 +1,7 @@
+package com.ugb.miprimerproyecto;
+
+public class utilidades {
+    static String url_consulta = "http://192.168.82.118:5984/db_amigos/_design/amigo/_view/_view";
+    static String url_mto = "http://192.168.82.118:5984/db_amigos/";
+
+}
